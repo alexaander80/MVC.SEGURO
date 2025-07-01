5ebf7436292c67202b064bc4cbdbeae72b9af218
alexaander30 <ghostanonimos137@gmail.com>
2025-07-01 11:44:20 -0500

    Agregar archivos de proyecto.

---
+++ WebAplplicasionUTNSeguro/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAplplicasionUTNSeguro.Models;
+
+namespace WebAplplicasionUTNSeguro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebAplplicasionUTNSeguro.Models.Mascota> Mascotas { get; set; } = default!;
+    }
+}
